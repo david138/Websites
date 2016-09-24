51cd5dd6cf63a1b8d93c5b1761ce7df77de23f83
Luke Davidson <Luke Davidson>
2016-09-23 20:03:38 -0400

    basic calculator and card randomizer

--- poker/script.js
+++ poker/script.js
@@ -11,14 +11,16 @@ function Card(num, suit) {
 
 }
 
+var randCard = new Card(-1, "r");
+
 function Player() {
     this.cards = new Array(2);
-    this.cards[0] = null;
-    this.cards[1] = null;
+    this.cards[0] = randCard;
+    this.cards[1] = randCard;
     this.inPlay = false;
 }
 
-var community = new Array(5).fill(null);
+var community = new Array(5).fill(randCard);
 
 function sumArrays(array1, array2) {
     var total = new Array(array1.length);
@@ -28,23 +30,17 @@ function sumArrays(array1, array2) {
     return total;
 }
 
-function add(a, b) {
-    return a + b;
-}
-
-
 /*
   Create the deck.
  */
 var deck = new Array(52);
 
 var symbols = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-var ranks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var suits = ["h", "c", "d", "s"];
 
 for (var i = 0; i < suits.length; i++) {
     for (var j = 0; j < symbols.length; j++) {
-        deck[i * symbols.length + j] = new Card(symbols[j], suits[i]);
+        deck[i * symbols.length + j] = new Card(j + 2, suits[i]);
     }
 }
 
@@ -58,11 +54,13 @@ for (var p = 0; p < players.length; p++) {
 players[0].inPlay = true;
 players[1].inPlay = true;
 
+var order = new Array(15)
+
 /*
   Returns the strength of a given hand.
  */
 function handStrength(hand) {
-    var order = new Array(15).fill(0);
+
     var straight = true;
     var flush = true;
     var i;
@@ -101,6 +99,8 @@ function handStrength(hand) {
         return [5, hand[4].num, hand[3].num, hand[2].num, hand[1].num, hand[0].num];
     }
 
+    order.fill(0);
+
     for (i = 0; i <= 4; i++) {
         order[hand[i].num] += 1;
     }
@@ -121,7 +121,7 @@ function handStrength(hand) {
         var lowerPair = order.indexOf(2);
         order[lowerPair] = 0;
         if (order.indexOf(2) != -1) {
-            return [2, lowerPair, order.indexOf(2), order.indexOf(1)]
+            return [2, order.indexOf(2), lowerPair, order.indexOf(1)]
         } else {
             var lowest = order.indexOf(1);
             order[lowest] = 0;
@@ -129,7 +129,7 @@ function handStrength(hand) {
             order[lowest2] = 0;
             var lowest3 = order.indexOf(1);
             order[lowest3] = 0;
-            return [1, lowest3, lowest2, lowest];
+            return [1, lowerPair, lowest3, lowest2, lowest];
         }
 
     }
@@ -143,11 +143,9 @@ function handStrength(hand) {
   or 2 if hand 2 is strong than hand 1.
   Returns -1 in case of tie.
  */
-function betterHand(hand1, hand2) {
-    var h1Strength = handStrength(hand1);
-    var h2Strength = handStrength(hand2);
-    var i;
-    for (i = 0; i < h1Strength.length; i++) {
+function betterHand(h1Strength, h2Strength) {
+
+    for (var i = 0; i < h1Strength.length; i++) {
         if (h1Strength[i] > h2Strength[i]) {
             return 1;
         }
@@ -159,156 +157,85 @@ function betterHand(hand1, hand2) {
 }
 
 /*
-  given an array of player cards and an array
-  of community cards, returns an array of
-  the 5 best cards.
+  given an array of cards, returns an array of
+  the best hand.
  */
-function bestHand (player, community)  {
+function bestHand (cards)  {
     var curBest = [-2];
 
-    var i, j;
+    console.log(cards);
 
-    for (i = 0; i < 6; i++) {
-        for (j = i + 1; j < 7; j++) {
-            var cards = player.concat(community);
-            cards.splice(i, 1);
-            cards.splice(j, 1);
-            if (betterHand(cards, curBest) == 1) {
-                curBest = cards;
+    for (var i = 0; i < 6; i++) {
+        for (var j = i + 1; j < 7; j++) {
+            var hand = cards.slice(0, i).concat(cards.slice(i + 1));
+            hand = hand.slice(0, j - 1).concat(hand.slice(j));
+            var cur = handStrength(hand);
+            if (betterHand(curBest, cur) == 2) {
+                curBest = cur;
             }
         }
     }
+
+    console.log(curBest);
     return curBest;
 }
 
-/*
-  given an array of players and an array of community cards
-  returns the index of the player with the best hand.
- */
-function winner (players, community) {
-
-    var winner = 0;
-    var i;
-
-    for (i = 1; i < players.length; i++) {
-        if (betterHand(bestHand(players[i - 1], community), bestHand(players[i])) == 2) {
-            winner = i;
-        }
+function CalculateWinPercentage (trials) {
+    var playersPlaying = [];
+    var wins = 0;
+    for (var i = 0; i < players.length; i++) {
+        console.log(players[i]);
+        players[i].inPlay ? playersPlaying.push( players[i] ) : null;
     }
-    return winner;
+    for (var i = 0; i < trials; i ++) {
+
+        for (var r = 0; r < playersPlaying.length; r++) {
+
+            !playersPlaying[r].cards[0].inPlay ? playersPlaying[r].cards[0] = randomCard() : null;
+            !playersPlaying[r].cards[1].inPlay ? playersPlaying[r].cards[1] = randomCard() : null;
+        }
+
+        for (var r = 0; r < community.length; r++) {
+            !community[r].inPlay ? community[r] = randomCard() : null;
+        }
+
+        var p1HandStrength = bestHand(players[0].cards.concat(community)),
+            p1Wins = true,
+            j = 1;
+        while (p1Wins && j < playersPlaying.length) {
+            betterHand(p1HandStrength, bestHand(players[j].cards.concat(community))) == 1 ? wins ++ : p1Wins = false;
+            j++;
+        }
+
+     }
+
+    for (var r = 0; r < playersPlaying.length; r++) {
+
+        !playersPlaying[r].cards[0].inPlay ? playersPlaying[r].cards[0] = randCard : null;
+        !playersPlaying[r].cards[1].inPlay ? playersPlaying[r].cards[1] = randCard : null;
+    }
+
+    for (var r = 0; r < community.length; r++) {
+        !community[r].inPlay ? community[r] = randCard : null;
+    }
+
+    return wins / trials * 100;
 }
 
-function handCalc() {
-    var winCount = playerCalc(0);
-    var sum = winCount.reduce(add, 0);
-
-    for (var i = 0; i < winCount.length; i++){
-        winCount[i] /= sum;
+function randomCard () {
+    var randomCard = Math.floor(Math.random() * 52);
+    while (deck[randomCard].inPlay) {
+        var randomCard = Math.floor(Math.random() * 52);
     }
-    return winCount;
+    return deck[randomCard];
 }
 
-function playerCalc(curPlayer) {
 
-    if (curPlayer == players.length - 1) {
-        return communityCalc(0, 0)
-    }
-
-    if (!players[curPlayer].inPlay) {
-        return playerCalc(curPlayer + 1);
-    }
-
-    if (players[curPlayer].cards[0] != null && players[curPlayer].cards[1] != null) {
-        return playerCalc(curPlayer + 1);
-    }
-
-    var total = new Array(players.length).fill(0);
-    var sum;
-
-    if (players[curPlayer].cards[0] == null && players[curPlayer].cards[1] == null) {
-        for (var i = 0; i < 52; i++) {
-            if (deck[i].inPlay == false) {
-                deck[i].inPlay = true;
-                players[curPlayer].cards[0] = deck[i];
-                for (var j = i + 1; j < 52; j++) {
-                    if (deck[j].inPlay == false) {
-                        deck[j].inPlay = true;
-                        players[curPlayer].cards[1] = deck[j];
-
-                        total =  sumArrays(total, playerCalc(curPlayer + 1));
-
-                        deck[j].inPlay = false;
-                        players[curPlayer].cards[0] = null;
-                    }
-                }
-
-                deck[i].inPlay = false;
-                players[curPlayer].cards[0] = null;
-            }
-        }
-    } else {
-
-        var missing;
-        if (players[curPlayer].cards[0] == null) {
-            missing = 0;
-        } else {
-            missing = 1;
-        }
-        for (var i = 0; i < 52; i++) {
-            if (deck[i].inPlay == false) {
-                deck[i].inPlay = true;
-                players[curPlayer].cards[missing] = deck[i];
-
-                total =  sumArrays(total, playerCalc(curPlayer + 1));
-
-                deck[i].inPlay = false;
-                players[curPlayer].cards[missing] = null;
-            }
-        }
-
-
-    }
-    return total;
-}
-
-function communityCalc(curCard, counter) {
-
-    if (community[curCard] == null) {
-        var total = new Array(players.length).fill(0);
-
-        for (var i = counter; i < 52; i++) {
-            if (deck[i].inPlay == false) {
-                community[curCard] = deck[i];
-                deck[i].inPlay = true;
-
-                if (curCard < 4) {
-                    total = sumArrays(total, communityCalc(curCard + 1, i + 1));
-                } else {
-                    total[winner(players, community)] += 1;
-                }
-                deck[i].inPlay = false;
-                community[curCard] = null;
-            }
-
-        }
-
-        return total;
-
-    } else {
-        if (curCard < 4) {
-            return communityCalc(curCard + 1, counter);
-        } else {
-            return new Array(players.length).fill(0)[winner(players, community)] ++;
-        }
-    }
-
-    return -1;
-}
 
 $(document).ready(function() {
     var i, j;
     for (i = 0; i < suits.length; i++) {
-        for (j = 0; j < ranks.length; j++) {
+        for (j = 0; j < symbols.length; j++) {
             var card = $('<div></div>');
             card.addClass(suits[i]  + " card " + i + j);
             //card.text(suits[i] + symbols[j]);
@@ -425,7 +352,103 @@ function cardPlaced() {
 
 $(document).ready(function() {
     $(".calculate").on("click", "button", function() {
-        $(this).after($("<p> hi </p>"));
-        console.log(handCalc());
+        $('.winrate').text(CalculateWinPercentage (10000));
     });
 });
+
+/*function playerCalc(curPlayer) {
+
+ if (curPlayer == players.length - 1) {
+ return communityCalc(0, 0)
+ }
+
+ if (!players[curPlayer].inPlay) {
+ return playerCalc(curPlayer + 1);
+ }
+
+ if (players[curPlayer].cards[0] != null && players[curPlayer].cards[1] != null) {
+ return playerCalc(curPlayer + 1);
+ }
+
+ var total = new Array(players.length).fill(0);
+ var sum;
+
+ if (players[curPlayer].cards[0] == null && players[curPlayer].cards[1] == null) {
+ for (var i = 0; i < 52; i++) {
+ if (deck[i].inPlay == false) {
+ deck[i].inPlay = true;
+ players[curPlayer].cards[0] = deck[i];
+ for (var j = i + 1; j < 52; j++) {
+ if (deck[j].inPlay == false) {
+ deck[j].inPlay = true;
+ players[curPlayer].cards[1] = deck[j];
+
+ total =  sumArrays(total, playerCalc(curPlayer + 1));
+
+ deck[j].inPlay = false;
+ players[curPlayer].cards[0] = null;
+ }
+ }
+
+ deck[i].inPlay = false;
+ players[curPlayer].cards[0] = null;
+ }
+ }
+ } else {
+
+ var missing;
+ if (players[curPlayer].cards[0] == null) {
+ missing = 0;
+ } else {
+ missing = 1;
+ }
+ for (var i = 0; i < 52; i++) {
+ if (deck[i].inPlay == false) {
+ deck[i].inPlay = true;
+ players[curPlayer].cards[missing] = deck[i];
+
+ total =  sumArrays(total, playerCalc(curPlayer + 1));
+
+ deck[i].inPlay = false;
+ players[curPlayer].cards[missing] = null;
+ }
+ }
+
+
+ }
+ return total;
+ }
+
+ function communityCalc(curCard, counter) {
+
+ if (community[curCard] == null) {
+ var total = new Array(players.length).fill(0);
+
+ for (var i = counter; i < 52; i++) {
+ if (deck[i].inPlay == false) {
+ community[curCard] = deck[i];
+ deck[i].inPlay = true;
+
+ if (curCard < 4) {
+ total = sumArrays(total, communityCalc(curCard + 1, i + 1));
+ } else {
+ total[winner(players, community)] += 1;
+ }
+ deck[i].inPlay = false;
+ community[curCard] = null;
+ }
+
+ }
+
+ return total;
+
+ } else {
+ if (curCard < 4) {
+ return communityCalc(curCard + 1, counter);
+ } else {
+ return new Array(players.length).fill(0)[winner(players, community)] ++;
+ }
+ }
+
+ return -1;
+ }*/
